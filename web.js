8ef5b6b7d94f7352ead8285a24ad1850afa805d9
Prabesh Bhaskaran <prabeshb@gmail.com>
2013-07-10 03:58:20 +0000

    Updated to read from file

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var readFile = function(filename) {
+    var buffer = new Buffer(fs.readFileSync(filename));
+    return buffer.toString("utf-8");
+}
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(readFile("index.html"));
 });
 
 var port = process.env.PORT || 5000;
